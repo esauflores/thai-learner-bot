3915fdbcd383dfaeccbc0fe52fa1d400db8ac0a7
César Esaú Flores Martínez <esauflores2022@gmail.com>
2024-05-09 15:45:55 +0700

    Feature Add card command

--- commands/add_card.py
+++ commands/add_card.py
@@ -1,6 +1,6 @@
-from helpers.cards_storage import CardsStorage, Card
-from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
-
+from telegram import Update, ReplyKeyboardRemove
+from helpers.global_storage import get_storage
+from helpers.cards_storage import Card
 
 # This command allows users to add new flashcards to the Flashcards project, helping them learn English to Thai translations.
 
@@ -18,9 +18,10 @@ from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 # /add_card Hello สวัสดี Greeting
 
 
-async def add_card(update: Update, context, card_storage: CardsStorage):
-    # Parse the command arguments
+async def add_card(update: Update, context):
+    # check the args
     args = context.args
+
     if len(args) < 2:
         await update.message.reply_text(
             "Invalid command. Please provide the English phrase and the Thai translation."
@@ -31,8 +32,12 @@ async def add_card(update: Update, context, card_storage: CardsStorage):
     thai_translation = args[1]
     tags = args[2:] if len(args) > 2 else []
 
+    # Get the storage
+
+    storage = get_storage()
+
     # Check if the card already exists
-    if card_storage.get_card_by_english_phrase(english_phrase) is not None:
+    if storage.get_card_by_english_phrase(english_phrase):
         await update.message.reply_text(
             "Card already exists. Please provide a different English phrase."
         )
@@ -40,7 +45,7 @@ async def add_card(update: Update, context, card_storage: CardsStorage):
 
     # Create a new card and add it to the storage
     card = Card(english_phrase, thai_translation, tags)
-    card_storage.add_card(card)
+    storage.add_card(card)
 
     await update.message.reply_text(
         "Flashcard added successfully.", reply_markup=ReplyKeyboardRemove()
