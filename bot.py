823003285f98a3429acc0e40467a0995a1f59d73
plinphon <greatez12345@gmail.com>
2024-05-10 12:50:47 +0700

    added start_quizz command pending waiting for message.

--- bot.py
+++ bot.py
@@ -11,7 +11,12 @@ from commands.add_card import add_card
 from commands.update_card import update_card
 from commands.get_card import get_card
 from commands.default_reply import default_reply
+from commands.start_quizz import start_quizz
 
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def main():
     bot_token = get_env_variable("BOT_TOKEN")
@@ -19,6 +24,8 @@ def main():
     print("Bot is running...")
 
     app = Application.builder().token(bot_token).build()
+    
+
 
     app.add_handler(
         CommandHandler(command="add_card", callback=add_card, has_args=True),
@@ -28,6 +35,7 @@ def main():
         CommandHandler(command="update_card", callback=update_card, has_args=True),
     )
 
+
     app.add_handler(
         CommandHandler(command="get_card", callback=get_card, has_args=True),
     )
@@ -35,10 +43,19 @@ def main():
     # app.add_handler(CommandHandler(command="update_card", callback=update_card))
 
     # app.add_handler(CommandHandler(command="get_card", callback=get_card))
+    
+    app.add_handler(
+        CommandHandler(command="start_quizz", callback=start_quizz, has_args=False),
+    )
 
-    app.add_handler(MessageHandler(filters=filters.TEXT, callback=default_reply))
+    app.add_handler(
+        MessageHandler(filters=filters.TEXT, callback=default_reply)
+    )
 
     app.run_polling()
+
+
+
 
 
 if __name__ == "__main__":
---
+++ commands/start_quizz.py
@@ -0,0 +1,51 @@
+import random
+from telegram import Update
+from telegram.ext import (
+    CommandHandler,
+    MessageHandler,
+    filters,
+    CallbackContext,
+)
+
+from helpers.global_storage import get_storage
+
+queue = [] 
+
+
+async def start_quizz(update: Update, context: CallbackContext):
+    global queue
+    
+   
+    storage = get_storage()
+    queue = storage.cards[:]
+    random.shuffle(queue)
+
+    # Start presenting questions
+    for card in queue:
+        await update.message.reply_text(
+            f"English phrase: {card.english_phrase}\nPlease provide the Thai translation."
+        )
+        user_response = None
+        while user_response is None:
+            updates = await context.bot.get_updates(update.update_id + 1)
+            print(updates)
+            # Check for new messages in the update object
+            for u in updates:
+                if u.message and u.message.text and not u.message.text.startswith('/'):
+                    user_response = u.message.text
+
+        if card.thai_translation == user_response:
+            
+            queue.pop(0)  # Remove the current question from the queue
+            
+            await update.message.reply_text("Correct! Let's get the next English phrase.")
+            
+        else:
+            # Move the current question to the end of the queue
+            queue.append(queue.pop(0))
+            await update.message.reply_text("Wrong answer. Let's get the next English phrase.")
+
+  
+    await update.message.reply_text("Quiz completed!")
+    return
+
