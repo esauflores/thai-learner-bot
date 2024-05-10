3323723864f34fd37ab688c0f32a88407381a66f
César Esaú Flores Martínez <esauflores2022@gmail.com>
2024-05-10 16:11:27 +0700

    Initialized with data

--- helpers/global_storage.py
+++ helpers/global_storage.py
@@ -1,6 +1,18 @@
-import helpers.cards_storage as cards_storage
+from helpers.cards_storage import Card, CardsStorage as cards_storage
 
-storage = {}
+storage = None
+
+
+def initialize_storage():
+    global storage
+
+    storage.add_card(Card("Hello", "สวัสดี", ["Greeting", "Vocabulary"]))
+    storage.add_card(Card("Goodbye", "ลาก่อน", ["Greeting", "Vocabulary"]))
+    storage.add_card(Card("Thank you", "ขอบคุณ", ["Vocabulary"]))
+
+    storage.add_card(Card("Cat", "แมว", ["Animal", "Pet"]))
+    storage.add_card(Card("Dog", "หมา", ["Animal", "Pet"]))
+    storage.add_card(Card("Elephant", "ช้าง", ["Animal"]))
 
 
 def get_storage():
@@ -8,5 +20,6 @@ def get_storage():
 
     if not storage:
         storage = cards_storage.CardsStorage()
+        initialize_storage()
 
     return storage
